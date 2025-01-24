885460ea5f5b9304695c68ce865b1dd983ce0c4c
Zhuoli Zhuang <zhuzhuan@cibci9.ihpc.uts.edu.au>
2025-01-24 12:30:52 +1100

    fix evaluation calculate metrics

--- eval/calculate_metrics.py
+++ eval/calculate_metrics.py
@@ -2,12 +2,21 @@ import os
 import json
 import pandas as pd
 import numpy as np
-
+import SimpleITK as sitk
+from utils.landmark.common import Landmark
+from utils.landmark.visualization.landmark_visualization_matplotlib import LandmarkVisualizationMatplotlib
 
 def calculate_identification_rate_and_l_error(gt_folder, pred_file, output_csv_path):
     # Read prediction JSON file
     with open(pred_file, 'r') as f:
         predictions = json.load(f)
+
+    vis = LandmarkVisualizationMatplotlib(dim=3,
+                                              annotations=dict([(i, f'C{i + 1}') for i in range(7)] +        # 0-6: C1-C7
+                                                               [(i, f'T{i - 6}') for i in range(7, 19)] +    # 7-18: T1-12
+                                                               [(i, f'L{i - 18}') for i in range(19, 25)] +  # 19-24: L1-6
+                                                               [(25, 'T13')]))                               # 25: T13
+
     results = []
     identification_rate_all = 0
     l_error_all = 0
@@ -41,6 +50,12 @@ def calculate_identification_rate_and_l_error(gt_folder, pred_file, output_csv_p
             [(label, *coords) for label, coords in subject_predictions],
             columns=['label', 'X', 'Y', 'Z']
         )
+        
+        landmarks = [Landmark(coords=coords, is_valid=True, value=label) for label, coords in subject_predictions]
+        
+        gt_landmarks = [Landmark(coords=[item['X'], item['Y'], item['Z']], is_valid=True, value=item['label']) for item in gt_data if 'label' in item]
+        #vis.visualize_landmark_projections(input_image, target_landmarks, filename=self.output_folder_handler.path_for_iteration(self.current_iter, current_id + '_landmarks_gt.png'))
+        #vis.visualize_prediction_groundtruth_projections(input_image, curr_landmarks_no_postprocessing, target_landmarks, filename=self.output_folder_handler.path_for_iteration(self.current_iter, current_id + '_landmarks.png'))
 
         # Merge ground truth and predictions based on labels
         merged_df = pd.merge(gt_df, pred_df, on='label', suffixes=('_gt', '_pred'))
@@ -70,9 +85,27 @@ def calculate_identification_rate_and_l_error(gt_folder, pred_file, output_csv_p
         })
 
     # Save results to a CSV file
-    results_df = pd.DataFrame(results)
-    results_df.to_csv(output_csv_path, index=False)
+    #results_df = pd.DataFrame(results)
+    #results_df.to_csv(output_csv_path, index=False)
 
+        # Path to the main directory containing multiple subfolders.
+        base_dir = "/projects/MAD3D/Zhuoli/MICCAI/VerSe2019/dataset-verse19test/rawdata/"
+        
+        
+        # Loop through all subdirectories and files.
+        for (i, folder) in enumerate(os.listdir(base_dir)):
+            for filename in os.listdir(os.path.join(base_dir, folder)):
+                # Check if the file ends with the desired pattern.
+                
+                if filename.endswith("ct.nii.gz") and folder == subject_id[4:]:
+                    file_path = os.path.join(base_dir, folder, filename)
+                    print(f"Reading: {file_path}")
+
+                    # Read using SimpleITK
+                    ct_image = sitk.ReadImage(file_path)
+                    vis.visualize_landmark_projections(ct_image, gt_landmarks, filename="/projects/MAD3D/Zhuoli/MICCAI/VerSe2019/dataset-verse19test/multi-view-predict/" +subject_id+ '_landmarks_gt.png')
+                    vis.visualize_landmark_projections(ct_image, landmarks, filename="/projects/MAD3D/Zhuoli/MICCAI/VerSe2019/dataset-verse19test/multi-view-predict/" +subject_id+ '_landmarks_pp.png')
+        
 # Example usage
 gt_folder_path = "/projects/MAD3D/Zhuoli/MICCAI/VerSe2019/dataset-verse19test/derivatives"
 prediction_file_path = "/projects/MAD3D/Zhuoli/MICCAI/clean_code/Multi-View-Vertebra-Localization-and-Identification-from-CT-Images/inference_save/fcn_test_voting.json"
